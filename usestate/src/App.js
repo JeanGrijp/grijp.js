c5df93edc608ee44b0b8281d765a02f6ce1e322a
Jean Grijp <40359722+JeanGrijp@users.noreply.github.com>
2022-03-30 17:03:43 -0300

    useState

---
+++ usestate/src/App.js
@@ -0,0 +1,29 @@
+import { Fragment, useEffect, useState } from "react";
+
+function App() {
+
+  const [count, setCount] = useState(0);
+  let num = 0
+
+  return (
+    <Fragment>
+      <h1>State: {count}</h1>
+      <h1>Const: {num}</h1>
+      <button onClick={() => setCount(count + 1)} >incrementar</button>
+      <button onClick={() => setCount(count - 1)} >decrementar</button>
+      <br/>
+      <br/>
+      <br/>
+      <button onClick={() => {
+        num = num + 1;
+        console.log(num)
+      }} >incrementar const</button>
+      <button onClick={() => {
+        num = num - 1;
+        console.log(num)
+      }} >decrementar const</button>
+    </Fragment>
+  );
+}
+
+export default App;
